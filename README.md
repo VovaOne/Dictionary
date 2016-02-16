08bd8f183d4b4f6cd4b102d8cad2b3905b590ed4
Vladimir Kiryakov <vovakiryakov@gmail.com>
2016-02-16 20:06:15 +0600

    added screen

---
+++ README.md
@@ -0,0 +1,4 @@
+###App to learn words by notification.
+
+![alt text](doc/screen1.png "")
+![alt text](doc/screen2.png "")
--- app/src/main/java/com/im/dictionary/view/activity/MainActivity.java
+++ app/src/main/java/com/im/dictionary/view/activity/MainActivity.java
@@ -17,12 +17,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import com.im.dictionary.App;
@@ -91,10 +91,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void initSwitchService() {
 
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        boolean notificationSwitchPosition = sharedPref.getBoolean(SERVICE_POWER_MODE_SHARED_PREFERENCE_PROPERTY, true);
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         View header = navigationView.getHeaderView(0);
+        SwitchCompat serviceSwitch = (SwitchCompat) header.findViewById(R.id.start_stop_service_switch);
+        serviceSwitch.setChecked(notificationSwitchPosition);
 
-        Switch serviceSwitch = (Switch) header.findViewById(R.id.start_stop_service_switch);
         serviceSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
